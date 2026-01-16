eaae17c24528df7089d098224d6d19e242f2ab0e
FabledFuryFox <tjweber2000@gmail.com>
2026-01-16 07:53:14 -0700

    Added Cards

---
+++ Assets/Scripts/CardValue.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public enum Suit
+{
+    Heart,
+    Spade,
+    Club,
+    Diamond
+}
+
+public class CardValue : MonoBehaviour
+{
+    [Range(2, 11)]
+    public int cardValue = 2;
+    
+    public Suit suit;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
